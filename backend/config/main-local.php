51c7891dbe40f28898798c7cf6b3b0282eacac42
orx <lbmzorx@163.com>
2017-10-13 10:07:00 +0800

    init project

---
+++ backend/config/main-local.php
@@ -0,0 +1,25 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'bk5ceI8jxy-4FV0Ekb44KVXd5P7ac_sD',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
---
+++ common/config/params.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'adminEmail' => 'lbmzorx@163.com',
+    'supportEmail' => 'support@example.com',
+    'user.passwordResetTokenExpire' => 3600,
+];
